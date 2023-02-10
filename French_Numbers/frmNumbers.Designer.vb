94b449be1fb4a717e0f25d9a3fa1688ab8e62151
K-c00per <katelyn.cooper@tntns.com>
2023-02-10 16:26:51 -0500

    Add project files.

---
+++ French_Numbers/frmNumbers.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmNumbers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblInstructions1 = New System.Windows.Forms.Label()
+        Me.lblInstructions2 = New System.Windows.Forms.Label()
+        Me.lblInstructions3 = New System.Windows.Forms.Label()
+        Me.btnOne = New System.Windows.Forms.Button()
+        Me.btnTwo = New System.Windows.Forms.Button()
+        Me.btnThree = New System.Windows.Forms.Button()
+        Me.btnFour = New System.Windows.Forms.Button()
+        Me.btnFive = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblFrench1 = New System.Windows.Forms.Label()
+        Me.lblFrench2 = New System.Windows.Forms.Label()
+        Me.lblFrench3 = New System.Windows.Forms.Label()
+        Me.lblFrench4 = New System.Windows.Forms.Label()
+        Me.lblFrench5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblInstructions1
+        '
+        Me.lblInstructions1.AutoSize = True
+        Me.lblInstructions1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!)
+        Me.lblInstructions1.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.lblInstructions1.Location = New System.Drawing.Point(36, 27)
+        Me.lblInstructions1.Name = "lblInstructions1"
+        Me.lblInstructions1.Size = New System.Drawing.Size(323, 18)
+        Me.lblInstructions1.TabIndex = 0
+        Me.lblInstructions1.Text = "Do you know the French words for the numbers"
+        '
+        'lblInstructions2
+        '
+        Me.lblInstructions2.AutoSize = True
+        Me.lblInstructions2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!)
+        Me.lblInstructions2.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.lblInstructions2.Location = New System.Drawing.Point(152, 45)
+        Me.lblInstructions2.Name = "lblInstructions2"
+        Me.lblInstructions2.Size = New System.Drawing.Size(90, 18)
+        Me.lblInstructions2.TabIndex = 1
+        Me.lblInstructions2.Text = "1 through 5?"
+        '
+        'lblInstructions3
+        '
+        Me.lblInstructions3.AutoSize = True
+        Me.lblInstructions3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!)
+        Me.lblInstructions3.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.lblInstructions3.Location = New System.Drawing.Point(74, 63)
+        Me.lblInstructions3.Name = "lblInstructions3"
+        Me.lblInstructions3.Size = New System.Drawing.Size(247, 18)
+        Me.lblInstructions3.TabIndex = 2
+        Me.lblInstructions3.Text = "Click the buttons below to see them."
+        '
+        'btnOne
+        '
+        Me.btnOne.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.btnOne.Location = New System.Drawing.Point(36, 96)
+        Me.btnOne.Name = "btnOne"
+        Me.btnOne.Size = New System.Drawing.Size(55, 28)
+        Me.btnOne.TabIndex = 3
+        Me.btnOne.Text = "1"
+        Me.btnOne.UseVisualStyleBackColor = True
+        '
+        'btnTwo
+        '
+        Me.btnTwo.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.btnTwo.Location = New System.Drawing.Point(103, 96)
+        Me.btnTwo.Name = "btnTwo"
+        Me.btnTwo.Size = New System.Drawing.Size(55, 28)
+        Me.btnTwo.TabIndex = 4
+        Me.btnTwo.Text = "2"
+        Me.btnTwo.UseVisualStyleBackColor = True
+        '
+        'btnThree
+        '
+        Me.btnThree.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.btnThree.Location = New System.Drawing.Point(170, 96)
+        Me.btnThree.Name = "btnThree"
+        Me.btnThree.Size = New System.Drawing.Size(55, 28)
+        Me.btnThree.TabIndex = 5
+        Me.btnThree.Text = "3"
+        Me.btnThree.UseVisualStyleBackColor = True
+        '
+        'btnFour
+        '
+        Me.btnFour.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.btnFour.Location = New System.Drawing.Point(237, 96)
+        Me.btnFour.Name = "btnFour"
+        Me.btnFour.Size = New System.Drawing.Size(55, 28)
+        Me.btnFour.TabIndex = 6
+        Me.btnFour.Text = "4"
+        Me.btnFour.UseVisualStyleBackColor = True
+        '
+        'btnFive
+        '
+        Me.btnFive.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.btnFive.Location = New System.Drawing.Point(304, 96)
+        Me.btnFive.Name = "btnFive"
+        Me.btnFive.Size = New System.Drawing.Size(55, 28)
+        Me.btnFive.TabIndex = 7
+        Me.btnFive.Text = "5"
+        Me.btnFive.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.btnExit.Location = New System.Drawing.Point(170, 182)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(55, 28)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblFrench1
+        '
+        Me.lblFrench1.BackColor = System.Drawing.Color.Turquoise
+        Me.lblFrench1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!)
+        Me.lblFrench1.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.lblFrench1.Location = New System.Drawing.Point(170, 138)
+        Me.lblFrench1.Name = "lblFrench1"
+        Me.lblFrench1.Size = New System.Drawing.Size(55, 28)
+        Me.lblFrench1.TabIndex = 9
+        Me.lblFrench1.Text = "un"
+        Me.lblFrench1.Visible = False
+        '
+        'lblFrench2
+        '
+        Me.lblFrench2.BackColor = System.Drawing.Color.Turquoise
+        Me.lblFrench2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFrench2.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.lblFrench2.Location = New System.Drawing.Point(170, 138)
+        Me.lblFrench2.Name = "lblFrench2"
+        Me.lblFrench2.Size = New System.Drawing.Size(55, 28)
+        Me.lblFrench2.TabIndex = 10
+        Me.lblFrench2.Text = "deux"
+        Me.lblFrench2.Visible = False
+        '
+        'lblFrench3
+        '
+        Me.lblFrench3.BackColor = System.Drawing.Color.Turquoise
+        Me.lblFrench3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!)
+        Me.lblFrench3.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.lblFrench3.Location = New System.Drawing.Point(170, 138)
+        Me.lblFrench3.Name = "lblFrench3"
+        Me.lblFrench3.Size = New System.Drawing.Size(55, 28)
+        Me.lblFrench3.TabIndex = 11
+        Me.lblFrench3.Text = "trois"
+        Me.lblFrench3.Visible = False
+        '
+        'lblFrench4
+        '
+        Me.lblFrench4.BackColor = System.Drawing.Color.Turquoise
+        Me.lblFrench4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!)
+        Me.lblFrench4.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.lblFrench4.Location = New System.Drawing.Point(170, 138)
+        Me.lblFrench4.Name = "lblFrench4"
+        Me.lblFrench4.Size = New System.Drawing.Size(55, 28)
+        Me.lblFrench4.TabIndex = 12
+        Me.lblFrench4.Text = "quatre"
+        Me.lblFrench4.Visible = False
+        '
+        'lblFrench5
+        '
+        Me.lblFrench5.BackColor = System.Drawing.Color.Turquoise
+        Me.lblFrench5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!)
+        Me.lblFrench5.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.lblFrench5.Location = New System.Drawing.Point(170, 138)
+        Me.lblFrench5.Name = "lblFrench5"
+        Me.lblFrench5.Size = New System.Drawing.Size(55, 28)
+        Me.lblFrench5.TabIndex = 13
+        Me.lblFrench5.Text = "cinq"
+        Me.lblFrench5.Visible = False
+        '
+        'frmNumbers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(394, 221)
+        Me.Controls.Add(Me.lblFrench5)
+        Me.Controls.Add(Me.lblFrench4)
+        Me.Controls.Add(Me.lblFrench3)
+        Me.Controls.Add(Me.lblFrench2)
+        Me.Controls.Add(Me.lblFrench1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnFive)
+        Me.Controls.Add(Me.btnFour)
+        Me.Controls.Add(Me.btnThree)
+        Me.Controls.Add(Me.btnTwo)
+        Me.Controls.Add(Me.btnOne)
+        Me.Controls.Add(Me.lblInstructions3)
+        Me.Controls.Add(Me.lblInstructions2)
+        Me.Controls.Add(Me.lblInstructions1)
+        Me.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmNumbers"
+        Me.Text = "French Numbers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblInstructions1 As Label
+    Friend WithEvents lblInstructions2 As Label
+    Friend WithEvents lblInstructions3 As Label
+    Friend WithEvents btnOne As Button
+    Friend WithEvents btnTwo As Button
+    Friend WithEvents btnThree As Button
+    Friend WithEvents btnFour As Button
+    Friend WithEvents btnFive As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblFrench1 As Label
+    Friend WithEvents lblFrench2 As Label
+    Friend WithEvents lblFrench3 As Label
+    Friend WithEvents lblFrench4 As Label
+    Friend WithEvents lblFrench5 As Label
+End Class
---
+++ French_Numbers/frmNumbers.vb
@@ -0,0 +1,67 @@
+﻿Public Class frmNumbers
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnOne.Click
+        lblFrench1.Visible = True
+        lblFrench2.Visible = False
+        lblFrench3.Visible = False
+        lblFrench4.Visible = False
+        lblFrench5.Visible = False
+
+    End Sub
+
+    Private Sub lblFrench2_Click(sender As Object, e As EventArgs) Handles lblFrench2.Click
+
+    End Sub
+
+    Private Sub lblFrench1_Click(sender As Object, e As EventArgs) Handles lblFrench1.Click
+
+    End Sub
+
+    Private Sub lblFrench3_Click(sender As Object, e As EventArgs) Handles lblFrench3.Click
+
+    End Sub
+
+    Private Sub lblFrench4_Click(sender As Object, e As EventArgs) Handles lblFrench4.Click
+
+    End Sub
+
+    Private Sub lblFrench5_Click(sender As Object, e As EventArgs) Handles lblFrench5.Click
+
+    End Sub
+
+    Private Sub btnTwo_Click(sender As Object, e As EventArgs) Handles btnTwo.Click
+        lblFrench1.Visible = False
+        lblFrench2.Visible = True
+        lblFrench3.Visible = False
+        lblFrench4.Visible = False
+        lblFrench5.Visible = False
+    End Sub
+
+    Private Sub btnThree_Click(sender As Object, e As EventArgs) Handles btnThree.Click
+        lblFrench1.Visible = False
+        lblFrench2.Visible = False
+        lblFrench3.Visible = True
+        lblFrench4.Visible = False
+        lblFrench5.Visible = False
+    End Sub
+
+    Private Sub btnFour_Click(sender As Object, e As EventArgs) Handles btnFour.Click
+        lblFrench1.Visible = False
+        lblFrench2.Visible = False
+        lblFrench3.Visible = False
+        lblFrench4.Visible = True
+        lblFrench5.Visible = False
+    End Sub
+
+    Private Sub btnFive_Click(sender As Object, e As EventArgs) Handles btnFive.Click
+        lblFrench1.Visible = False
+        lblFrench2.Visible = False
+        lblFrench3.Visible = False
+        lblFrench4.Visible = False
+        lblFrench5.Visible = True
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+
+    End Sub
+End Class
